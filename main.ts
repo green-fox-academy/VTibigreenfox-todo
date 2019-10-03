4dd6fc311f6ac27402d620bdb22beab009e5e841
Varga Tibor <varga.tibor.at18@gmail.com>
2019-10-03 20:07:39 +0200

    first task

--- main.ts
+++ main.ts
@@ -4,13 +4,20 @@ const fs = require ('fs'); //ez micsoda?
 const args: string[] = process.argv; 
 
 const printTxt: string = fs.readFileSync('printusage.txt', 'utf-8');
+let listTask: string = fs.readFileSync('listtask.txt', 'utf-8');
+let newArray: string[] = [];
+newArray.push(listTask)
+
 let mainProcess = () => {
   if (args.length == 2) {
-    console.log('this is without arguments, help me!');
-  } else if (args[2] == '-l') {
     console.log(printTxt);
+  } 
+  else if (args[2] == '-l') {
+    for(let i = 1; i<newArray.length+1; i++){
+    
+    }
+    console.log(' ' + listTask);
   }
-   
 }
 
 mainProcess();
